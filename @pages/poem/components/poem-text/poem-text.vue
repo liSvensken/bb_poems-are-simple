6a38c72b12688ebc22226e5b837013eeb5181f2a
eli3avetta <eli3avetta.shvedchikova@gmail.com>
2022-05-01 13:51:54 +0300

    save

--- @pages/poem/components/poem-text/poem-text.vue
+++ @pages/poem/components/poem-text/poem-text.vue
@@ -2,6 +2,10 @@
 import { Component, Vue } from "nuxt-property-decorator";
 import { poemResponse } from "~/@pages/poem/components/poem-text/fake-api-response";
 
+/**
+ * v-tooltip - на будущее, когда будет не только оригинальный текст
+ * **/
+
 @Component({
   components: {}
 })
@@ -9,7 +13,7 @@ import { poemResponse } from "~/@pages/poem/components/poem-text/fake-api-respon
 export default class extends Vue {
 
   mounted() {
-  	(this.$refs.poemText as HTMLElement).innerHTML = poemResponse;
+    (this.$refs.poemText as HTMLElement).innerHTML = poemResponse.original;
   }
 };
 </script>
@@ -32,27 +36,6 @@ export default class extends Vue {
       />
     </div>
 
-    <div class="poem-text-wrapper" ref="poemText">
-<!--      <p>Последняя туча <span class="mark-blue" v-tooltip="{-->
-<!--       trigger: 'click hover',-->
-<!--       content: 'Чувство неизвестности, тревожно радостное ожидание, вера, в лучший исход'-->
-<!--      }-->
-<!--      ">рассеянной</span> бури!</p>-->
-<!--      <p>Одна ты несешься по ясной лазури,</p>-->
-<!--      <p>Одна ты наводишь унылую тень,</p>-->
-<!--      <p>Одна ты <span class="mark-orange" v-tooltip="'несильная, но распространяющаяся по большой территории'">печалишь ликующий день</span>.-->
-<!--      </p>-->
-<!--      <p>Ты небо недавно кругом облегала,</p>-->
-<!--      <p>И молния грозно тебя обвивала;</p>-->
-<!--      <p>И ты издавала таинственный гром</p>-->
-<!--      <p>И алчную землю поила дождем.</p>-->
-<!--      <p>Довольно, сокройся! Пора миновалась,</p>-->
-<!--      <p>Земля освежилась, и буря промчалась,</p>-->
-<!--      <p>И ветер, лаская листочки древес,</p>-->
-<!--      <p>Тебя с успокоенных гонит небес.</p>-->
-<!--      <br/>-->
-<!--      <br/>-->
-<!--      <p>Год написания: 1835</p>-->
-    </div>
+    <div class="poem-text-wrapper" ref="poemText"/>
   </div>
 </template>
